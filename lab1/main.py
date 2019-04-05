fd313096aa55050f49a6c6564f865473b9ea4588
robij1996 <robert.stanik1996@gmail.com>
2019-04-05 16:17:25 +0200

    Sprawozdanie

--- lab1/main.py
+++ lab1/main.py
@@ -6,30 +6,37 @@ from scheduler import Job, Scheduler
 
 if __name__ == '__main__':
 
-    # for path in os.listdir('data'):
-    #     job_data = np.loadtxt(f'data/{path}', dtype=int, skiprows=1 )
-    #     jobs = [Job(job_id, times) for job_id, times in enumerate(job_data)]
 
-    #     print(path)
-    #     sched = Scheduler(jobs)
-    #     makespan, job_order = sched.neh_algorithm()
-    #     print('Total makespan: ', makespan)
-    #     print('Job order: ', job_order)
-    #     makespan, job_order = sched.neh_algorithm(improvement=1)
-    #     print('Total makespan: ', makespan)
-    #     print('Job order: ', job_order)
-
-        job_data = np.loadtxt(f'data/ta020.txt', dtype=int, skiprows=1 )
+    for path in os.listdir('data'):
+        job_data = np.loadtxt(f'data/{path}', dtype=int, skiprows=1 )
         jobs = [Job(job_id, times) for job_id, times in enumerate(job_data)]
         sched = Scheduler(jobs)
+        # print(path)
+        # makespan, job_order = sched.neh_sorted_list()
+        # # print("(",len(jobs),len(jobs[0].times),")")
+        # #print('Total makespan NEH: ', makespan)
+        
+        # # print('Job order NEH: ', job_order)
 
-        makespan, job_order = sched.neh_sorted_list()
-        print('Total makespan: ', makespan)
-        print('Job order: ', job_order)
+        # f = sched.neh_algorithm()
+        makespan, job_order = sched.sa(jobs)
+        print(makespan)
+        # print('Job order SA: ', job_order)
 
-        f = sched.neh_algorithm()
-        makespan, job_order = sched.sa(f)
-        print('Total makespan: ', makespan)
-        print('Job order: ', job_order)
+    
+        # job_data = np.loadtxt(f'data/ta100.txt', dtype=int, skiprows=1 )
+        # jobs = [Job(job_id, times) for job_id, times in enumerate(job_data)]
+        # sched = Scheduler(jobs)
+        
+        # makespan, job_order = sched.neh_sorted_list()
+        # # print('Total makespan: ', makespan)
+        # # print('Job order: ', job_order)
+        
+        # f = sched.neh_algorithm()
+        # makespan, job_order = sched.sa(f)
+        # print(makespan)
+        # print('Total makespan: ', makespan)
+        # print('Job order: ', job_order)
+
    
 
