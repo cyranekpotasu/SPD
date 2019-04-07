3c95da526adf549beb865152612d93ca4fd3f264
cyranekpotasu <cyran.norbert97@gmail.com>
2019-04-07 21:52:45 +0200

    Analyze ignoring equal cmaxes

--- lab1/main.py
+++ lab1/main.py
@@ -19,17 +19,21 @@ if __name__ == '__main__':
     #     print('Total makespan: ', makespan)
     #     print('Job order: ', job_order)
 
-        job_data = np.loadtxt(f'data/ta020.txt', dtype=int, skiprows=1 )
-        jobs = [Job(job_id, times) for job_id, times in enumerate(job_data)]
-        sched = Scheduler(jobs)
+    job_data = np.loadtxt('data/ta020.txt', dtype=int, skiprows=1)
+    jobs = [Job(job_id, times) for job_id, times in enumerate(job_data)]
+    sched = Scheduler(jobs)
 
-        makespan, job_order = sched.neh_sorted_list()
-        print('Total makespan: ', makespan)
-        print('Job order: ', job_order)
+    makespan, job_order = sched.neh_sorted_list()
+    print('Total makespan: ', makespan)
+    print('Job order: ', job_order)
 
-        f = sched.neh_algorithm()
-        makespan, job_order = sched.sa(f)
-        print('Total makespan: ', makespan)
-        print('Job order: ', job_order)
-   
-
+    sched.jobs = sched.neh_algorithm()
+    makespan, job_order = sched.sim_annealing()
+    print('Total makespan: ', makespan)
+    print('Job order: ', job_order)
+    makespans = [sched.sim_annealing()[0] for _ in range(5)]
+    print('Avg of 5 runs: ',
+          sum(makespan for makespan in makespans) / len(makespans))
+    makespans = [sched.sim_annealing(accept_equal=False)[0] for _ in range(5)]
+    print('Avg of 5 runs (ignore equal cmaxes): ',
+          sum(makespan for makespan in makespans) / len(makespans))
