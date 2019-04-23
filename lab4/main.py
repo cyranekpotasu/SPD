44b205fde3b6c1a0e2229467f0833592956ec1d8
cyranekpotasu <cyran.norbert97@gmail.com>
2019-04-23 22:36:37 +0200

    Check Schrage algorithm for every data set

---
+++ lab4/main.py
@@ -0,0 +1,12 @@
+import os
+
+import numpy as np
+
+from lab4.scheduler import Job, schrage_algorithm
+
+if __name__ == '__main__':
+    for path in os.listdir('data'):
+        job_data = np.loadtxt(f'data/{path}', dtype=int, skiprows=1)
+        jobs = [Job(job_id, *times) for job_id, times in enumerate(job_data, 1)]
+        perm = schrage_algorithm(jobs)
+        print(f'Perm for {path}: {perm}')
